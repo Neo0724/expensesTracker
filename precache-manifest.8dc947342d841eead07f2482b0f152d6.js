5141d9a872081cd91539898f37420439f16bf0b1
github_Neo0724 <neoben007@gmail.com>
2023-05-18 20:56:19 +0800

    Updates

---
+++ precache-manifest.8dc947342d841eead07f2482b0f152d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08cba4485459700ae537",
+    "url": "/expensesTracker/static/css/main.c8262a02.chunk.css"
+  },
+  {
+    "revision": "08cba4485459700ae537",
+    "url": "/expensesTracker/static/js/main.08cba448.chunk.js"
+  },
+  {
+    "revision": "87a880cc4a9ddc34b1b5",
+    "url": "/expensesTracker/static/js/1.87a880cc.chunk.js"
+  },
+  {
+    "revision": "f8957a8095e79ef16ac2",
+    "url": "/expensesTracker/static/js/runtime~main.f8957a80.js"
+  },
+  {
+    "revision": "545cea763517c5e20e58bb948039fd99",
+    "url": "/expensesTracker/index.html"
+  }
+];
